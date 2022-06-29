67f2ed66e21bea60ee704efc1a1d9dff22c68471
kalyan16190 <kalyankumar16190@gmail.com>
2022-06-29 18:24:38 +0530

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,28 @@
 import react from 'react';
 import { useForm } from 'react-hook-form';
-
 import './App.css';
-
 
 function App() {
   const onSubmit = (data:any)=>{console.log(data)}
-const {register, formState:{errors}, handleSubmit}
- = useForm()  
- 
- return (
+  const {register, formState:{errors}, handleSubmit}= useForm()  
+  return (
     <div className="App">
+      <div className='form-App'>
       <form onSubmit={handleSubmit(onSubmit)}>
-      <label htmlFor="UserName">User Name:</label>
-        <input {...register("UserName", {required:true})}type="text"/><br/>
-        {errors.UserName?.type === 'required' && "UserName is required"}<br/>
+      <label htmlFor="UserName">UserName</label>
+        <input {...register("UserName", {required:true})}type="text" name='UserName'/>
+        {errors.UserName?.type === 'required' && "UserName is required"}
         
       <label htmlFor="email">Email :</label>
-        <input {...register("email", {required:true})}type="email"/><br/>
-        {errors.email?.type === 'required' && "email is required"}<br/>
+        <input {...register("email", {required:true})}type="email" name='email'/>
+        {errors.email?.type === 'required' && "email is required"}
         
         <label htmlFor="password">Password:</label> 
-        <input {...register("password", {required:true})} type="password"/><br/>
-        {errors.password?.type === 'required' && "Password is required"}<br/>
-        <input type='submit' name='Submit'  />
+        <input {...register("password", {required:true})} type="password" name='password'/>
+        {errors.password?.type === 'required' && "Password is required"}
+        <button type='submit' name='Submit'>Submit</button>
        </form>
-      
+       </div>
     </div>
   );
 }
